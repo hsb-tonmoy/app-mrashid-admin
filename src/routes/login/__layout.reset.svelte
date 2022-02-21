8d1abf74e483330cd9d08448073e8807190c040b
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-02-21 12:50:26 -0500

    Initial commit

---
+++ src/routes/login/__layout.reset.svelte
@@ -0,0 +1,15 @@
+<script>
+	const registerBg2 = '../assets/img/register_bg_2.png';
+</script>
+
+<div>
+	<main>
+		<section class="flex items-center relative w-full h-full py-40 min-h-screen">
+			<div
+				class="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+				style="background-image: url({registerBg2});"
+			/>
+			<slot />
+		</section>
+	</main>
+</div>
