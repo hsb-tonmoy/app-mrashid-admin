f77ed12144e9026942a0442b602520eace9061a2
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-06-20 14:38:55 -0400

    Added document download button.

--- src/lib/DocumentSubmission/DocumentDetail/DocumentPreview.svelte
+++ src/lib/DocumentSubmission/DocumentDetail/DocumentPreview.svelte
@@ -7,16 +7,24 @@
 	class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0"
 >
 	<div class="rounded-t bg-white mb-0 px-6 py-6">
-		<div class="flex flex-col">
+		<div class="flex justify-between items-center">
+			<div class="flex flex-col">
+				<a
+					href={`/dashboard/document-submission/${data.student_data.id}`}
+					class="flex items-center gap-x-2 mb-3 text-xs text-gray-500 hover:text-black"
+					><i class="fa-solid fa-arrow-left" />Return to document list</a
+				>
+				<h1 class="text-blueGray-700 text-xl font-bold">{data.title}</h1>
+				<p class="text-gray-600 text-sm">
+					{data.category.name}, uploaded {convertDate(data.uploaded_at, false)}
+				</p>
+			</div>
 			<a
-				href={`/dashboard/document-submission/${data.student_data.id}`}
-				class="flex items-center gap-x-2 mb-3 text-xs text-gray-500 hover:text-black"
-				><i class="fa-solid fa-arrow-left" />Return to document list</a
+				href={data.document}
+				download={data.document}
+				class="bg-blue-600 text-white hover:bg-blue-700 font-bold uppercase text-xs px-3 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+				><i class="fas fa-download mr-2" /> Download</a
 			>
-			<h1 class="text-blueGray-700 text-xl font-bold">{data.title}</h1>
-			<p class="text-gray-600 text-sm">
-				{data.category.name}, uploaded {convertDate(data.uploaded_at, false)}
-			</p>
 		</div>
 	</div>
 	<div class="flex justify-center px-4 lg:px-10 py-10">
